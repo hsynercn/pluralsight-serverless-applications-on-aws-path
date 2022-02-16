3ca777648c6a304814b966ef3f0a355503a1ee96
Huseyin Can Ercan <huscanercan@gmail.com>
2022-02-16 23:16:30 +0300

    Add access empty methods

--- src/my_versions/ps-serverless-app/services/comments/index.js
+++ src/my_versions/ps-serverless-app/services/comments/index.js
@@ -8,3 +8,36 @@ import { AWSClients, generateID } from '../common';
 //Utilize the DynamoDB Document Client
 const dynamoDB = AWSClients.dynamoDB();
 const tableName = process.env.DYNAMO_DB_TABLE;
+
+const getAllCommentsForDocument = async (request, response) => {
+
+}
+
+const createComment = async (request, response) => {
+
+}
+
+const deleteComment = async (request, response) => {
+
+}
+
+const router = createRouter(RouterType.HTTP_API_V2);
+
+router.add(
+    Matcher.HttpApiV2('GET', '/comments/(:docid)'),
+    getAllCommentsForDocument,
+);
+
+router.add(
+    Matcher.HttpApiV2('GET', '/comments/(:docid)'),
+    createComment,
+);
+
+router.add(
+    Matcher.HttpApiV2('GET', '/comments/(:docid)/(:commentid)'),
+    deleteComment,
+);
+
+exports.handler = async (event, context) => {
+    return router.run(event, context);
+}
