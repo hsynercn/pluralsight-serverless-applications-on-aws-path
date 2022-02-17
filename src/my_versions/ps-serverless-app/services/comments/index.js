0003d54a61d54d01694d6f561846fcc0344c3933
Huseyin Can Ercan <huscanercan@gmail.com>
2022-02-17 09:03:55 +0300

    Implemented comment services

--- src/my_versions/ps-serverless-app/services/comments/index.js
+++ src/my_versions/ps-serverless-app/services/comments/index.js
@@ -10,15 +10,47 @@ const dynamoDB = AWSClients.dynamoDB();
 const tableName = process.env.DYNAMO_DB_TABLE;
 
 const getAllCommentsForDocument = async (request, response) => {
-
+    const params = {
+        TableName: tableName,
+        KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
+        ExpressionAttributeValues: {
+            ':pk': request.pathVariables.docid,
+            ':sk': 'Comment'
+        }
+    };
+    const results = await dynamoDB.query(params).promise();
+    return response.output(results.Items, 200);
 }
 
 const createComment = async (request, response) => {
-
+    const userId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+    const commentId = `Comment${generateID()}`;
+    const item = {
+        PK: request.pathVariables.docid,
+        SK: commentId,
+        DateAdded: new Date().toISOString(),
+        Owner: userId,
+        ...JSON.parse(request.event.body),
+    }
+    const params = {
+        TableName: tableName,
+        Item: item,
+        ReturnValues: 'NONE'
+    };
+    await dynamoDB.put(params).promise();
+    return response.output(item, 200);
 }
 
 const deleteComment = async (request, response) => {
-
+    const params = {
+        TableName: tableName,
+        Key: {
+            PK: request.pathVariables.docid,
+            SK: `Comment#${request.pathVariables.commentId}`,
+        },
+    };
+    await dynamoDB.delete(params).promise();
+    return response.output({}, 200);
 }
 
 const router = createRouter(RouterType.HTTP_API_V2);
